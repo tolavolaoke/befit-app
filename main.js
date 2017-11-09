18b849324ba4af9852d73e247ed2868c61f71dbd
tolavolaoke <tolavolaoke@hotmail.co.uk>
2017-11-09 22:20:17 +0000

    My First Commit creating the folder

---
+++ main.js
@@ -0,0 +1,15 @@
+// console.log("Hi!");
+
+// var lastName = "Omotola Olaoke"; 
+// var thirdLetterOfLastName = lastName[2];
+// console.log(thirdLetterOfLastName);
+
+var melon=document.getElementById("watermelon");
+
+	melon.addEventListener("click", myFunction);
+
+function moveWatermelon() {
+    document.getElementsByClassName("blue")[0].innerHTML += "<img src='https://vignette3.wikia.nocookie.net/clubpenguin/images/f/f9/Watermelon_Pin.PNG/revision/latest?cb=20140323201318'>";
+
+}
+
